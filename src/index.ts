e1fd80e26c722b77a5f07ea53d24cf78cf7fe84c
mlaskowski <maciejlaskowsky@gmail.com>
2019-11-20 23:40:14 +0100

    init repository

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 100, -20]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data)
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection);
