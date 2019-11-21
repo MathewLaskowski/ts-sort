1fbda6e17dac8ea2d6071b9aa7f55fe151168123
mlaskowski <maciejlaskowsky@gmail.com>
2019-11-21 18:24:14 +0100

    add LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 100, -20]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data)
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
+charactersCollection.sort();
 console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
